ae53bee1fb7503a46ce7ea71c23c0dfcaa70ad94
Emirhan Kızmaz <133758059+KizmazEmirhan@users.noreply.github.com>
2024-06-27 20:22:44 +0300

    Added Tailwind
    Tailwind css added and some files configured for it.

--- Todo-App/todo-list/src/components/TodoList.vue
+++ Todo-App/todo-list/src/components/TodoList.vue
@@ -1,50 +1,71 @@
 <template>
-  <div class="todo-app">
-    <div class="todo-container">
-      <div class="add-task-box">
-        <h2>Add New Task</h2>
-        <div class="add-task">
-          <input
-            type="text"
-            class="add-task-input"
-            placeholder="Add your task here!"
-            v-model="newTask"
-            @keyup.enter="addTask(newTask)"
-          />
-          <button class="add-btn" @click="addTask(newTask)">
-            <span class="material-icons">add</span>
-          </button>
-        </div>
-        <div class="current-date">
-          {{ formattedDate }}
-        </div>
-      </div>
-      <div class="tasks-box">
-        <h2>Tasks</h2>
-        <div class="tasks-section">
-          <div
-            class="task"
-            v-for="(task, index) in tasks"
-            :key="index"
-            :style="{ backgroundColor: task.color }"
-          >
-            <span :class="{ 'task-title': true, hidden: task.editing }">
-              {{ task.title }}
-            </span>
+  <div
+    class="flex flex-col lg:flex-row justify-center items-center min-h-screen p-5 bg-gray-100"
+  >
+    <div class="w-full max-w-5xl bg-white shadow-lg rounded-lg overflow-hidden">
+      <div class="flex flex-col lg:flex-row">
+        <div
+          class="flex-1 flex flex-col items-center p-5 border-b-2 lg:border-b-0 lg:border-r-2 border-gray-200 bg-teal-50"
+        >
+          <h2 class="mb-5 text-xl font-bold text-teal-700">Add New Task</h2>
+          <div class="flex items-center w-full">
             <input
-              v-if="task.editing"
-              class="task-item-editing"
-              v-model="task.title"
-              @keyup.enter="doneUpdateTask(task)"
-              ref="editInput"
+              type="text"
+              class="flex-1 p-4 border border-teal-700 rounded-full text-lg mr-2 focus:outline-none focus:ring-2 focus:ring-teal-700"
+              placeholder="Add your task here!"
+              v-model="newTask"
+              @keyup.enter="addTask(newTask)"
             />
-            <div class="task-icons">
-              <button class="icon-btn edit-btn" @click="updateTask(task)">
-                <span class="material-icons">edit</span>
-              </button>
-              <button class="icon-btn remove-btn" @click="removeTask(index)">
-                <span class="material-icons">delete</span>
-              </button>
+            <button
+              class="flex items-center justify-center w-12 h-12 bg-teal-700 rounded-full text-white hover:scale-110 transform transition-transform duration-200"
+              @click="addTask(newTask)"
+            >
+              <span class="material-icons">add</span>
+            </button>
+          </div>
+          <div class="mt-5 text-lg text-teal-700">
+            {{ formattedDate }}
+          </div>
+        </div>
+        <div class="flex-2 p-5 bg-green-50 w-full">
+          <h2 class="mb-5 text-xl font-bold text-green-800">Tasks</h2>
+          <div class="space-y-4">
+            <div
+              class="flex items-center justify-between p-4 rounded-lg shadow-sm transition-shadow duration-200 transform hover:shadow-md hover:scale-105"
+              v-for="(task, index) in tasks"
+              :key="index"
+              :style="{ backgroundColor: task.color }"
+            >
+              <span
+                :class="{
+                  'flex-grow text-lg text-gray-800 mr-2': true,
+                  hidden: task.editing,
+                }"
+              >
+                {{ task.title }}
+              </span>
+              <input
+                v-if="task.editing"
+                class="w-3/5 p-2 border border-green-800 rounded-lg text-lg"
+                v-model="task.title"
+                @keyup.enter="doneUpdateTask(task)"
+                ref="editInput"
+                @keyup.esc="doneUpdateTask(task)"
+              />
+              <div class="flex space-x-2">
+                <button
+                  class="flex items-center justify-center w-9 h-9 bg-yellow-500 rounded-full text-white hover:scale-110 transform transition-transform duration-200"
+                  @click="updateTask(task)"
+                >
+                  <span class="material-icons">edit</span>
+                </button>
+                <button
+                  class="flex items-center justify-center w-9 h-9 bg-red-600 rounded-full text-white hover:scale-110 transform transition-transform duration-200"
+                  @click="removeTask(index)"
+                >
+                  <span class="material-icons">delete</span>
+                </button>
+              </div>
             </div>
           </div>
         </div>
@@ -167,6 +188,11 @@ export default {
   },
 };
 </script>
+
 <style>
-@import "../styles/todo-styles.css";
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap");
+@import url("https://fonts.googleapis.com/icon?family=Material+Icons");
 </style>
